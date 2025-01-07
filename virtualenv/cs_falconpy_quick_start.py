f0b7e08707b167f170ce7ac8ed6fe1167acd8257
mmartello <mmartello@prismvisiongroup.com>
2025-01-07 10:32:05 -0500

    minor changes

--- virtualenv/cs_falconpy_quick_start.py
+++ virtualenv/cs_falconpy_quick_start.py
@@ -2,6 +2,7 @@
 import os
 from dotenv import load_dotenv
 from falconpy import Hosts
+from art import text2art
 
 # Load env variables
 load_dotenv()
@@ -13,8 +14,7 @@ load_dotenv()
 # Instead, provide these values as variables that are retrieved from the environment,
 # read from an encrypted file or secrets store, provided at runtime, etc.
 
-hosts = Hosts(client_id=os.getenv("FALCON_CLIENT_ID"), 
-              client_secret=os.getenv("FALCON_CLIENT_SECRET"))
+hosts = Hosts(client_id=os.getenv("FALCON_CLIENT_ID"), client_secret=os.getenv("FALCON_CLIENT_SECRET"))
 
 # While this example retrieves credentials from the environment as the variables
 # "FALCON_CLIENT_ID" and "FALCON_CLIENT_SECRET". Developers leveraging environment
@@ -22,40 +22,58 @@ hosts = Hosts(client_id=os.getenv("FALCON_CLIENT_ID"),
 #
 # hosts = Hosts()
 
-# SEARCH_FILTER = "hostname-search-string"
-SEARCH_FILTER = 'revenue-cbo-0092'
+def __search_Hosts(host):
+    # SEARCH_FILTER = "hostname-search-string"
+    SEARCH_FILTER = 'SEC-ENG-CBO'
 
-# Retrieve a list of hosts that have a hostname that matches our search filter
-hosts_search_result = hosts.query_devices_by_filter(filter=f"hostname:*'*{SEARCH_FILTER}*'")
+    # Retrieve a list of hosts that have a hostname that matches our search filter
+    hosts_search_result = host.query_devices_by_filter(filter=f"hostname:*'*{SEARCH_FILTER}*'")
 
-# Confirm we received a success response back from the CrowdStrike API
-if hosts_search_result["status_code"] == 200:
-    hosts_found = hosts_search_result["body"]["resources"]
-    # Confirm our search produced results
-    if hosts_found:
-        # Retrieve the details for all matches
-        hosts_detail = hosts.get_device_details(ids=hosts_found)["body"]["resources"]
-        print(hosts_detail)
-        for detail in hosts_detail:
-            # Display the AID and hostname for this match
-            aid = detail["device_id"]
-            hostname = detail["hostname"]
-            status = detail["status"]
-            mac_address = detail["mac_address"]
-            first_seen = detail["first_seen"]
-            connection_ip = detail["connection_ip"]
-            chassis_type_desc = detail["chassis_type_desc"]
-            system_manufacturer = detail["system_manufacturer"]
-            os_product_name = detail["os_product_name"]
-
-            print(f"\nHostname: {hostname}\naid: ({aid})\nStatus: {status}\nMac Address: {mac_address}\nFirst Seen: {first_seen}\nConnection IP: {connection_ip}\nChassis Type: {chassis_type_desc}\nSystem Manufacturer: {system_manufacturer}\nOperating Sytem: {os_product_name}\n")
+    # Confirm we received a success response back from the CrowdStrike API
+    if hosts_search_result["status_code"] == 200:
+        hosts_found = hosts_search_result["body"]["resources"]
+        # Confirm our search produced results
+        if hosts_found:
+            # Retrieve the details for all matches
+            hosts_detail = host.get_device_details(ids=hosts_found)["body"]["resources"]
+            return hosts_detail
+            # for detail in hosts_detail:
+            #     # Display the AID and hostname and more for this match
+            #     aid = detail["device_id"]
+            #     hostname = detail["hostname"]
+            #     print(f"\nHostname: {hostname}\naid: ({aid})")
+        else:
+            print("No hosts found matching that hostname within your Falcon tenant.")
     else:
-        print("No hosts found matching that hostname within your Falcon tenant.")
-else:
-    # Retrieve the details of the error response
-    error_detail = hosts_search_result["body"]["errors"]
-    for error in error_detail:
-        # Display the API error detail
-        error_code = error["code"]
-        error_message = error["message"]
-        print(f"[Error {error_code}] {error_message}")
+        # Retrieve the details of the error response
+        error_detail = hosts_search_result["body"]["errors"]
+        for error in error_detail:
+            # Display the API error detail
+            error_code = error["code"]
+            error_message = error["message"]
+            print(f"[Error {error_code}] {error_message}")
+
+# Recursion Method to travse unknonwn depth of JSON object
+def __recursive_Object(data, indent=int(0)):
+    # Dictionary check
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print(" " * indent + str(key) + ":")
+            __recursive_Object(value, indent + 2)
+    # List check
+    elif isinstance(data, list):
+        for item in data:
+            __recursive_Object(item, indent)
+    else:
+        print(" " * indent + str(data))
+
+# Main Method
+def main():
+    print(text2art("Falcon API", font="small"))
+    print("Searching......\n")
+    output = __recursive_Object(__search_Hosts(hosts), 0)
+    # output = __search_Hosts(hosts)
+    return output
+
+if __name__ == '__main__':
+    main()
